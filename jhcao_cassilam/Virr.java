2289fae4746edbf3698e7841ac4e10fc9adafca2
cassilam <rm.cassilam@gmail.com>
2018-03-12 12:58:37 -0700

    Added correct code

--- jhcao_cassilam/Virr.java
+++ jhcao_cassilam/Virr.java
@@ -1,4 +1,4 @@
-package Virr;
+package jhcao_cassilam;
 import robocode.*; 
 import java.awt.Color;
 
@@ -13,23 +13,46 @@ public class Virr extends AdvancedRobot
 	 * run: Virr's default behavior
 	 */
 	double prevEnergy = 100.00; //initializes prevEnergy for onScannedRobot
+	int turn = 1; 
 	public void run() {
 		setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-		int turn = 1; 
 		// Robot main loop
 		for (int i = 0; i < 100; i++){
 			setTurnGunLeft(99999); 
 			setTurnGunRight(99999); 
 		} //scans for robots
-	}
+	   
+      	// Wall Avoidance
+     	if (getHeading() > 315 && getHeading() <= 45) {
+      		if (getY() + 45 == getBattleFieldHeight()) {
+           		setTurnLeft(45);
+         	}
+      	} else if (getHeading() <= 315 && getHeading() > 225) {
+        	if (getX() == 45) {
+            	setTurnLeft(45);
+        	}
+      	} else if (getHeading() <= 215 && getHeading() > 135) {
+        	if (getY() == 45) {
+            	setTurnLeft(45);
+         	}
+      	} else if (getHeading() <= 135 && getHeading() > 45) {
+        	if (getX() + 45 == getBattleFieldWidth()) {
+            	setTurnLeft(45);
+         	}
+      	}
+
+      
+   }
+
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	//create shooting strategies: head on, circular, pattern
 	public void onScannedRobot(ScannedRobotEvent e) { 
-		int turn = 1;
-		if ((turn%2) == 0){
+		// Original Author: Jae Marsh
+     	// https://www.ibm.com/developerworks/library/j-dodge/
+      	if ((turn%2) == 0){
 			setTurnLeft(e.getBearing() + 90); //turns to the right to make it easier to
 		} else {
 			setTurnLeft(e.getBearing() - 90); 
@@ -91,6 +114,5 @@ public class Virr extends AdvancedRobot
 		setTurnGunLeft(720); 
 		scan(); 
 	}	
-	//create method/algorithn for preventing bot from running into walls (bc running into walls = loss of power and possibility of getting stuck)
-
+   
 }
